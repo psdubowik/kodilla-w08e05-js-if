ff1a92f77eb7488c54d44a0b7bc2cea99f2d2964
PSD <psdubowik@gmail.com>
2017-10-04 13:57:00 +0200

    Adding spaces and changing operator

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var a = 10;
 var b = -10;
-var value=(a * a) - (2 * a * b) + (b * b);
+var value = (a * a) - (2 * a * b) + (b * b);
 console.log(value);
 
-if (value>0) {
+if (value > 0) {
     console.log("Wynik dodatni");
-} else if (value==0) {
+} else if (value === 0) {
     console.log("Wynik równy zero");
-} else if (value<0) {
+} else if (value < 0) {
     console.log("Wynik ujemny");
 } else {
     console.log("Wprowadź wartości liczbowe");
